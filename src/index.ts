d6a9e58e44d439405b5a2dd5afd5e13f46b32f2b
Guilhermo Masid <guilhermomasid@gmail.com>
2020-11-22 20:12:23 -0300

    set locale of generated index.js to /dist and not /src anymore

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.send('Deu certo')
+  return response.send('Deu certooo')
 })
 
 app.listen(3333)
